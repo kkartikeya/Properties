6370f93a65a861d0b8a97cdee769e17e8608284a
KK <kk@retailnext.net>
2016-04-26 16:50:28 -0700

    Updating Test Case, its not a complete test case

--- src/test/java/com/kkartikeya/home/Properties/ReadPropertiesTest.java
+++ src/test/java/com/kkartikeya/home/Properties/ReadPropertiesTest.java
@@ -1,5 +1,7 @@
 package com.kkartikeya.home.Properties;
 
+import java.util.Properties;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,7 +9,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class ReadPropertiesTest 
     extends TestCase
 {
     /**
@@ -15,7 +17,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public ReadPropertiesTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +27,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( ReadPropertiesTest.class );
     }
 
     /**
@@ -34,5 +36,8 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+        ReadProperties readProp = new ReadProperties();
+        
+        Properties prop = readProp.readProperties();
     }
 }
